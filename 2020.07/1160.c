b5d00e3bbda37a351464c0565f2f26c27cf531b6
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-07-09 21:19:06 +0900

    1160 : 아르바이트 가는 날

---
+++ 2020.07/1160.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() 
+{
+	int a;
+	scanf_s("%d", &a);
+
+	switch (a) {
+	case 1:
+		printf("oh my god"); break;
+	case 2:
+		printf("enjoy"); break;
+	case 3:
+		printf("oh my god"); break;
+	case 4:
+		printf("enjoy"); break;
+	case 5:
+		printf("oh my god"); break;
+	case 6:
+		printf("enjoy"); break;
+	case 7:
+		printf("oh my god");
+	}
+	return 0;
+}
