a274ec7379f946be5cc0d4e567749ad560b2593b
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-07-29 02:11:41 +0900

    1098 : [기초-2차원배열] 설탕과자 뽑기

---
+++ 2020.06/1098.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[101][101];  
+
+    int h, w, n, l, d, x, y;
+
+    scanf_s("%d %d", &h, &w); 
+    scanf_s("%d", &n);     
+    for (int i = 1; i <= h; i++) 
+    {
+        for (int j = 1; j <= w; j++) 
+        {
+            a[i][j] = 0; 
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+
+        scanf_s("%d %d %d %d", &l, &d, &x, &y);
+        if (d == 0) {
+            for (int j = 0; j < l; j++) {
+                a[x][y + j] = 1;
+            }
+        }
+        else {
+            for (int j = 0; j < l; j++) {
+                a[x + j][y] = 1;
+            }
+        }
+    }
+
+    for (int i = 1; i <= h; i++) 
+    {
+        for (int j = 1; j <= w; j++) 
+        {
+            printf("%d ", a[i][j]); 
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
