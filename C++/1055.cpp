7e6ef6270ba15c068aa9e367ebf82a4d57c75bed
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-08-05 18:00:26 +0900

    1055 : [기초-논리연산] 하나라도 참이면 참 출력하기(설명)

---
+++ C++/1055.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long int a,b;
+	cin >> a;
+	cin >> b;
+
+	if (a==1 || b==1) {
+		cout << 1 << endl;
+	}
+	else {
+		cout << 0 << endl;
+	}
+	
+}
