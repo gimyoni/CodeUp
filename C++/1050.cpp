dac8ca978d83c112e746ffc4ac05e9fbc77a804b
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-08-05 17:53:22 +0900

    1050 : [기초-비교연산] 두 정수 입력받아 비교하기2(설명)

---
+++ C++/1050.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long int a,b;
+	cin >> a;
+	cin >> b;
+	if (a == b) {
+		cout << 1 << endl;
+	}
+	else {
+		cout << 0 << endl;
+	}
+	
+}
