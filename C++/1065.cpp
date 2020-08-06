578e24143f63858597a7756afa5044b5954d4655
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-08-06 17:24:35 +0900

    1065 : [기초-조건/선택실행구조] 정수 3개 입력받아 짝수만 출력하기(설명)

---
+++ C++/1065.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long int a,b,c;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	if (a % 2 == 0) {
+		cout << a << endl;
+	}
+	if (b % 2 == 0) {
+		cout << b << endl;
+	}
+	if (c % 2 == 0) {
+		cout << c << endl;
+	}
+}
