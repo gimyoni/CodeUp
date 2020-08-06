4333128cc2c13d75ea444dad238b89c361e0c5c2
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-08-06 17:26:39 +0900

    1066 : [기초-조건/선택실행구조] 정수 3개 입력받아 짝/홀 출력하기(설명)

---
+++ C++/1066.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long int a,b,c;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	if (a % 2 == 0) {
+		cout << "even" << endl;
+	}
+	else {
+		cout << "odd" << endl;
+	}
+
+	if (b % 2 == 0) {
+		cout << "even" << endl;
+	}
+	else {
+		cout << "odd" << endl;
+	}
+
+	if (c % 2 == 0) {
+		cout << "even" << endl;
+	}
+	else {
+		cout << "odd" << endl;
+	}
+
+	return 0;
+}
