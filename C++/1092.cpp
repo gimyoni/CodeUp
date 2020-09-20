084ba3b595dd84287383760df581b5680e31095e
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-09-20 13:40:23 +0900

    1092 : [기초-종합] 함께 문제 푸는 날(설명)

---
+++ C++/1092.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c,day=1;
+    cin>>a>>b>>c;
+    while(true){
+        if(day%a==0 and day%b==0 and day%c==0){cout<<day; break;}
+        else{day+=1;}
+    }
+}
