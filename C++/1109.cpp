88e6860b76846df05bebb5b5c6c47b1c4cf28734
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-09-23 20:58:58 +0900

    1109 : 보안카드 접수증

---
+++ C++/1109.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char name[20];
+    int age;
+    char code;
+    double key;
+    
+    cin>>name;
+    cin>>age;
+    cin>>code;
+    cin>>key;
+    
+    cout<<name<<endl;
+    cout<<age<<endl;
+    cout<<code<<endl;
+    cout<<key<<endl;
+  
+    
+}
