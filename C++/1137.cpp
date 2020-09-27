d3701784273e29533dab3eb52188eefd070a5506
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-09-27 20:06:33 +0900

    1137 : 관계연산자 3

---
+++ C++/1137.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    
+    cin>>a;
+    cin>>b;
+    
+    if(a==b){
+        cout<<"0"<<endl;
+    }else{
+        cout<<"1"<<endl;
+    }
+    
+    return 0;
+}
