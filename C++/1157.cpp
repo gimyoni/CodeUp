a9006dfc5c4efbae723c12e7b570f432124e04e6
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-10-05 15:42:37 +0900

    1157 : 특별한 공 던지기 1

---
+++ C++/1157.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{  
+    double a;
+   
+    cin>> a;
+    
+    if(a>=50 && a<=60){
+        cout<<"win"<<endl;
+    }else{
+        cout<<"lose"<<endl;
+    }
+    return 0;
+}
