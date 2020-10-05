cb0e7b3f6e5d6f58b853a2a865fb3cb6274a41ca
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-10-05 15:38:48 +0900

    1155 : 7의 배수

---
+++ C++/1155.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{  
+    int a;
+   
+    cin>> a;
+    
+    if(a%7==0){
+        cout<<"multiple"<<endl;
+    }else{
+        cout<<"not multiple"<<endl;
+    }
+    return 0;
+}
