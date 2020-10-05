dbea7062c3f76626340fcdf9f29b14db50f6ae5a
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-10-05 15:46:55 +0900

    1160 : 아르바이트 가는 날

---
+++ C++/1160.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() 
+{
+	int a;
+	cin>>a;
+
+	switch (a) {
+	case 1:
+		cout<<"oh my god"<<endl; break;
+	case 2:
+		cout<<"enjoy"<<endl; break;
+	case 3:
+		cout<<"oh my god"<<endl; break;
+	case 4:
+		cout<<"enjoy"<<endl; break;
+	case 5:
+	    cout<<"oh my god"<<endl; break;
+	case 6:
+		cout<<"enjoy"<<endl; break;
+	case 7:
+		cout<<"oh my god"<<endl; break;
+	}
+	return 0;
+}
