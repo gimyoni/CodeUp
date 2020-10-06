851a3896bec5951c28641f65a13000e1cbfc99a3
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-10-06 20:35:12 +0900

    1169 : 나이 계산 2

---
+++ C++/1169.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{  
+    int a;
+    
+    cin>>a;
+    
+    if (a<=13) {
+        cout<< 13 -a<<" 3"<<endl;
+    }
+    else {
+        cout<< 101-a+12<<" 1"<<endl;
+    }
+}
