45b14367d28a5ecfed91fa1d034acafaefe160ea
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-10-06 19:17:44 +0900

    1162 : 당신의 사주를 봐 드립니다 1

---
+++ C++/1162.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int a, b, c;
+	cin>>a;
+	cin>>b;
+	cin>>c; 
+	
+	if ((a - b + c) % 10 == 0)
+	{
+	    cout<<"대박"<<endl;
+	}
+	else {
+		cout<<"그럭저럭"<<endl;
+	}
+	return 0;
+}
