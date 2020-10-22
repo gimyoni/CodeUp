694fcd4cfd9f7f5e742102828c5324cc6ab0d909
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-10-23 08:57:52 +0900

    1253 : a 부터 b까지 출력하기

---
+++ C/1253.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+ 
+int main(){
+    
+    int a,b,temp;
+    scanf("%d %d", &a, &b);
+    
+    if(a > b){
+        temp = a;
+        a = b;
+        b = temp;
+    }
+ 
+    for(int i = a; i <= b; i++){
+        printf("%d ",i);
+    }
+    
+    
+    return 0;
+}
