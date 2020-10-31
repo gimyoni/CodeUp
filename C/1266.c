04893ebf4b7e7a031e98dfdd642461cbb9ec6537
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-10-31 22:18:55 +0900

    1266 : n개의 수의 합

--- C/1266.c
+++ C/1266.c
@@ -2,10 +2,10 @@
 int main(){
     int num, num2, sum=0;
     scanf("%d", &num);
-    for(int i=0; i<num2; i++){
+    for(int i=0; i<num; i++){
         scanf("%d",&num2);
         sum += num2;
     }
-    printf("%d",sum)
+    printf("%d",sum);
     
 }
