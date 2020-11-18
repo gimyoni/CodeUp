0ba828350e0626dd0e4ba6517eea49d720317cdd
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-11-18 19:00:28 +0900

    1126 : 정수 계산기

---
+++ Py/1126.py
@@ -0,0 +1,6 @@
+a,b = map(int, input().split())
+print(str(a)+" + "+str(b)+" = "+str(a+b))
+print(str(a)+" - "+str(b)+" = "+str(a-b))
+print(str(a)+" * "+str(b)+" = "+str(a*b))
+print(str(a)+" / "+str(b)+" = "+str(a//b))
+print(str(a)+" % "+str(b)+" = "+str(a%b))
