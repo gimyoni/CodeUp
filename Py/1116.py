7a664b43147e8fda04cb0dea70a3597fc09a0f31
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-11-18 18:27:23 +0900

    1116 : 사칙연산 계산기

---
+++ Py/1116.py
@@ -0,0 +1,5 @@
+a,b = map(int, input().split())
+print(str(a)+"+"+str(b)+"="+str(a+b))
+print(str(a)+"-"+str(b)+"="+str(a-b))
+print(str(a)+"*"+str(b)+"="+str(a*b))
+print(str(a)+"/"+str(b)+"="+str(a//b))
