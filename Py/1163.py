02cbeedbbcbc15557968d6a405c04f849c372b60
YeonHui Kim <56942588+gimyoni@users.noreply.github.com>
2020-11-20 23:45:35 +0900

    1163 : 당신의 사주를 봐 드립니다 2

---
+++ Py/1163.py
@@ -0,0 +1,11 @@
+a, b, c= map(int,input().split())
+
+if (a+b+c)%100 == 0:
+    print("대박")
+else:
+    print("그럭저럭")a, b, c= map(int,input().split())
+
+if ((a+b+c)//100)%2 == 0:
+    print("대박")
+else:
+    print("그럭저럭")
